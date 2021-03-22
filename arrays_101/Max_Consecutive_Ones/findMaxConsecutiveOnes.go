31198cffea5ee37229d37deebb33e25b365766d1
Dmitry Vandyshev <dmit.van@gmail.com>
2021-03-22 09:12:17 +0500

    Refactor arrays_101/Max_Consecutive_Ones

--- arrays_101/Max_Consecutive_Ones/findMaxConsecutiveOnes.go
+++ arrays_101/Max_Consecutive_Ones/findMaxConsecutiveOnes.go
@@ -3,16 +3,15 @@
 package main
 
 func findMaxConsecutiveOnes(nums []int) int {
-	var max_ones int
-	var cur_ones int
+	max_ones, cur_ones := 0, 0
 	for _, v := range nums {
 		if v == 1 {
 			cur_ones++
-			if cur_ones > max_ones {
-				max_ones = cur_ones
-			}
 		} else {
 			cur_ones = 0
+		}
+		if cur_ones > max_ones {
+			max_ones = cur_ones
 		}
 	}
 	return max_ones
