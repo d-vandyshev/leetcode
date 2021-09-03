f5416d4add4caaf126d9c501d5e2e66920c6217d
Dmitry Vandyshev <dmit.van@gmail.com>
2021-09-03 10:46:00 +0300

    Add arrays_101/Duplicate_Zeros

---
+++ arrays_101/Duplicate_Zeros/duplicateZeros.go
@@ -0,0 +1,14 @@
+package main
+
+func duplicateZeros(arr []int) []int {
+	for i := 0; i < len(arr) - 1; i++ {
+		if arr[i] == 0 {
+			for j := len(arr) - 1; j > i + 1; j-- {
+				arr[j] = arr[j - 1]
+			}
+			arr[i + 1] = 0
+			i++
+		}
+	}
+	return arr
+}
