574dda30548e6451754d7b4a4692277b1ce89f32
Dmitry Vandyshev <dmit.van@gmail.com>
2021-09-05 19:30:10 +0300

    Add arrays_101/Merge_Sorted_Array

---
+++ arrays_101/Merge_Sorted_Array/merge.go
@@ -0,0 +1,10 @@
+package main
+
+func merge(nums1 []int, m int, nums2 []int, n int)  []int {
+   for _, n2 := range nums2 {
+		  for i := 0; i < len(nums1); i++ {
+			  if nums1[i] 
+
+		  }
+	 }
+}
