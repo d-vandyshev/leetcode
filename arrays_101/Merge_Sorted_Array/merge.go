1adcfb30cc0ab894c655a988f5f9dbb54cbc2a4a
Dmitry Vandyshev <dmit.van@gmail.com>
2021-09-06 08:24:22 +0300

    Add solution for arrays_101/Merge_Sorted_Array

--- arrays_101/Merge_Sorted_Array/merge.go
+++ arrays_101/Merge_Sorted_Array/merge.go
@@ -1,10 +1,21 @@
 package main
 
 func merge(nums1 []int, m int, nums2 []int, n int)  []int {
-   for _, n2 := range nums2 {
-		  for i := 0; i < len(nums1); i++ {
-			  if nums1[i] 
-
-		  }
-	 }
+	for j, n2 := range nums2 {
+		nums2_item_used := false
+		for i := 0; i < m + j; i++ {
+			if nums1[i] > n2 {
+				for z := m + n - 1; z > i; z-- {
+					nums1[z] = nums1[z - 1]
+				}
+				nums1[i] = n2
+				nums2_item_used = true
+				break
+			}
+		}
+		if !nums2_item_used {
+			nums1[m + j] = n2
+		}
+	}
+	return nums1
 }
