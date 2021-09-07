f183eab19a7d43a05289f704b5df7c91439459c7
Dmitry Vandyshev <dmit.van@gmail.com>
2021-09-07 20:19:24 +0300

    Add test for Linked_List/Design_Linked_List

---
+++ Linked_List/Design_Linked_List/MyLinkedList_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestMyLinkedList(t *testing.T) {
+	myLinkedList := Constructor()
+	myLinkedList.AddAtHead(1)
+	myLinkedList.AddAtTail(3)
+	myLinkedList.AddAtIndex(1, 2) // linked list becomes 1->2->3
+	val := myLinkedList.Get(1)
+	if myLinkedList.Get(1) != 2 {
+		t.Error("Test1 expected", 2, "got", val)
+	}
+	myLinkedList.DeleteAtIndex(1) // now the linked list is 1->3
+	val = myLinkedList.Get(1)
+	if val != 3 {
+		t.Error("Test2 expected", 3, "got", val)
+	}
+}
