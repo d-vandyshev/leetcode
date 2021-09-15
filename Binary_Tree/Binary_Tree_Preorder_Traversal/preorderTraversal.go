82d713e6f1561322b7f498fb1ab8c525409e2838
Dmitry Vandyshev <dmit.van@gmail.com>
2021-09-15 04:34:09 +0300

    Add Binary_Tree/Binary_Tree_Preorder_Traversal

---
+++ Binary_Tree/Binary_Tree_Preorder_Traversal/preorderTraversal.go
@@ -0,0 +1,49 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	traversal := make([]int, 0, 1000)
+	if root == nil {
+			return traversal
+	}
+	node := root
+
+	breadcrumbs := make([]*TreeNode, 0, 1000)
+	var prev_node *TreeNode
+	var reverse bool
+	reverse = false
+
+	for i := 0; ; i++ {
+		if !reverse {
+			breadcrumbs = append(breadcrumbs, node)
+			traversal = append(traversal, node.Val)
+		}
+		// fmt.Printf("Node address: %p\n", node)
+		// fmt.Println("    i:" , i, "node:", node, "address: ", &node, "traversal:", traversal, "breadcrumbs:", breadcrumbs)
+		// fmt.Println("    reverse:" , reverse)
+		if node.Left != nil && !reverse {
+			node = node.Left
+			reverse = false
+			continue
+		}
+		if node.Right != nil && prev_node != node.Right {
+			node = node.Right
+			prev_node = nil
+			reverse = false
+			continue
+		}
+		if len(breadcrumbs) <= 1 {
+			break
+		}
+		breadcrumbs = breadcrumbs[:len(breadcrumbs) - 1]
+		prev_node = node
+		node = breadcrumbs[len(breadcrumbs) - 1]
+		reverse = true
+	}
+	return traversal
+}
--- Binary_Tree/Binary_Tree_Preorder_Traversal/preorderTraversal_test.go
+++ Binary_Tree/Binary_Tree_Preorder_Traversal/preorderTraversal_test.go
@@ -9,31 +9,33 @@ type testcase struct {
 	traversal []int
 }
 
-func arr2tree(arr []int, index int) *Node {
-	if index >= len(arr) {
-		return &Node{val: -1000}
+func arr2tree(arr []int, index int) *TreeNode {
+	if len(arr) == 0 {
+		return nil
 	}
-	node := Node{val: arr[index]}
+	if index >= len(arr) {
+		return &TreeNode{Val: -1000}
+	}
+	node := TreeNode{Val: arr[index]}
 
 	index_left := index * 2 + 1
 	if index_left < len(arr) && arr[index_left] != -1000 {
-		node.left = arr2tree(arr, index_left)
+		node.Left = arr2tree(arr, index_left)
 	}
 
 	index_right := index * 2 + 2
 	if index_right < len(arr) && arr[index_right] != -1000 {
-		node.right = arr2tree(arr, index_right)
+		node.Right = arr2tree(arr, index_right)
 	}
 	return &node
 }
 
 var tests = []testcase{
-	{[]int{10,20,30,40,50,-1000,70,-1000,-1000,100,110,120}, []int{10,20,40,50,100,110,30,70,120}},
-	{[]int{1, -1000, 2, 3}, []int{1, 2, 3}},
+	{[]int{10,20,30, 40,50,-1000,70, -1000,-1000,100,110,-1000,-1000,120,-1000}, []int{10,20,40,50,100,110,30,70,120}},
+	{[]int{1, -1000, 2, -1000, -1000, 3, -1000}, []int{1, 2, 3}},
 	{[]int{}, []int{}},
 	{[]int{1}, []int{1}},
 	{[]int{1, 2}, []int{1, 2}},
-	{[]int{1, -1, 2}, []int{1, 2}},
 }
 
 func Equal(a, b []int) bool {
@@ -49,12 +51,9 @@ func Equal(a, b []int) bool {
 }
 
 func TestPreorderTraversal(t *testing.T) {
-	for i, test := range tests {
-		if i > 0 {
-			break
-		}
+	for _, test := range tests {
 		tree := arr2tree(test.arr, 0)
-		result_arr := preorderTraversal(*tree)
+		result_arr := preorderTraversal(tree)
 		if !Equal(result_arr, test.traversal) {
       t.Error("For", test.arr, "expected", test.traversal, "got", result_arr)
 		}
